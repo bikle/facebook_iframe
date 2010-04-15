5ca0e7fe1e7184c420be27be2b0cd059dd953159
bikle <bikle@bikle.com>
2010-04-15 10:46:49 -0700

    c6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,7 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  filter_parameter_logging :fb_sig_friends
+  before_filter :ensure_application_is_installed_by_facebook_user
+
 end
