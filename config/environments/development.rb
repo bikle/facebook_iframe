dc55374698ca15411c853a296f7e5d33c721381f
Dan Bikle <bikle@bikle.com>
2010-04-15 17:25:32 -0700

    Added vendor/gems/rails-footnotes-3.6.6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,6 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# Nice javascript links at bottom of pages
+config.gem 'rails-footnotes', :lib => 'rails-footnotes', :source => 'http://gemcutter.org', :version => '>=3.6.3'
