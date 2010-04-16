a8de5f47540f49a9d7fbf8b40ad96216a1770de6
Dan Bikle <bikle@bikle.com>
2010-04-15 17:26:24 -0700

    Enhanced routing and application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,15 @@ class ApplicationController < ActionController::Base
   # filter_parameter_logging :password
   filter_parameter_logging :fb_sig_friends
   before_filter :ensure_application_is_installed_by_facebook_user
+  before_filter :facebook_variables
+
+  # Define some useful variables
+  def facebook_variables
+    @facebooker_config    = Facebooker::AdapterBase.facebooker_config
+    @canvas_page_name     = @facebooker_config['canvas_page_name']
+    @application_base_url = 'http://apps.facebook.com/' + @canvas_page_name
+    @api_key              = @facebooker_config['api_key']
+    @ajax_server          = "#{Facebooker::AdapterBase.facebooker_config['callback_url']}"
+  end
 
 end
