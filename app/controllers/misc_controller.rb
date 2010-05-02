6cb8d6070c8b1a1520a0662be9147e40e9c961a7
Dan Bikle <bikle@bikle.com>
2010-05-02 16:44:16 -0700

    Enhanced write2news_feed.haml.  It can post to NF now.

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,17 +1,28 @@
 class MiscController < ApplicationController
   def write2news_feed
-    # I will publish to myself first
+debugger
+
+    # fb:multi-friend-input gives us params["ids"].
+    # If params["ids"] is nil, just return.  This just shows the form.
+    return if params["ids"].nil?
+
+    # Get my id from facebook_session
     fbuid= facebook_session.user.uid
     fb_sender= Facebooker::User.new(fbuid)
-    fb_recipient= Facebooker::User.new(fbuid)
     begin
-      FbPublisher.deliver_publish_nf(fb_sender, fb_recipient, {:message => "This is a test of write2news_feed()"})
+      # Put myself at the head of the list.
+      # fb:multi-friend-input may provide more fb-ids via params["ids"]
+      fb_recipients_ids= ([fbuid]+ params["ids"]).flatten.uniq
+      # Loop through each id. Be careful, we might have a nil in there (we should not but check anyway).
+      fb_recipients_ids.each{ |fbid|
+        unless fbid.nil?
+          fb_recipient= Facebooker::User.new(fbid)
+          FbPublisher.deliver_publish_nf(fb_sender, fb_recipient, {:message => "This is a test of write2news_feed(): #{params['msg4nf']}"})
+        end # unless fbid.nil?
+      }
     rescue Facebooker::Session::PermissionError
       @facebooker_session_permissionerror= true
-      flash[:error]= "We have a Facebooker::Session::PermissionError.  We can fix this error by going to this URL: http://apps.facebook.com/#{@canvas_page_name}/hello/permissions"
-    end
-
-    # Publish to the others
+    end # begin, rescue
 
   end # def write2news_feed
 
