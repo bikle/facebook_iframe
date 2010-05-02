17492e052cda35afa23bacfd627dca1996cec287
Dan Bikle <bikle@bikle.com>
2010-05-02 13:29:41 -0700

    Working on demo of write2news_feed.haml and Facebooker::Rails::Publisher

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,5 +1,19 @@
 class MiscController < ApplicationController
   def write2news_feed
-  end
+    # I will publish to myself first
+    fbuid= facebook_session.user.uid
+    fb_sender= Facebooker::User.new(fbuid)
+    fb_recipient= Facebooker::User.new(fbuid)
+    begin
+      FbPublisher.deliver_publish_nf(fb_sender, fb_recipient, {:message => "This is a test of write2news_feed()"})
+    rescue Facebooker::Session::PermissionError
+      @facebooker_session_permissionerror= true
+      flash[:error]= "We have a Facebooker::Session::PermissionError.  We can fix this error by going to this URL: http://apps.facebook.com/#{@canvas_page_name}/hello/permissions"
+    end
 
-end
+    # Publish to the others
+
+  end # def write2news_feed
+
+end # class MiscController
+
