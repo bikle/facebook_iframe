4efdca8cd094093dbbc459c17b7507a71202a108
Dan Bikle <bikle@bikle.com>
2010-05-10 10:45:40 -0700

    Enhanced English in /misc_controller.rb, jq_scrape_form.haml

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -32,10 +32,12 @@ class MiscController < ApplicationController
   def jq_scrape
     require 'open-uri'
     @myurl= params[:myurl]
-    hdrs = {"User-Agent"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1", "Accept-Charset"=>"utf-8", "Accept"=>"text/html"}
-    my_html = ""
+    hdrs= {"User-Agent"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1", "Accept-Charset"=>"utf-8", "Accept"=>"text/html"}
+    my_html= ""
     open(@myurl, hdrs).each {|s| my_html << s}
+    # This works on the whiteboard but not here:
     # @my_html= my_html.sub(/.+<body/,'<body').sub(/<\/body>.+$/,'</body>')
+    # Dont worry, jQuery will eventually toss everything in the trash.
     @my_html= my_html
   end # def jq_scrape
 
